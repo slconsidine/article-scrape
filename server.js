f74c703ebdaa6ddaa4f578fdb99c4c13cd9fc3fb
Sam <samlconsidine@gmail.com>
2020-04-23 14:38:04 -0400

    Updated routes for retriving/updating notes for specific articles

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ app.get("/scrape", function(req, res) {
     .then(function(response) {
         var $ = cheerio.load(response.data);
 
-
         $(".item-container h2").each(function(i, element) {
             var result = {};
             result.title = $(this)
@@ -32,6 +31,51 @@ app.get("/scrape", function(req, res) {
             result.date = $(this)
                 .children("p .publish-date")
                 .text();
-        })
-    })
-})
+
+            db.Article.create(result)
+            .then(function(dbArticle) {
+                console.log(dbArticle);
+            }).catch(function(err) {
+                console.log(err);
+            });
+        });
+        res.send("Scrape Complete");
+    });
+});
+
+// get route for showing all articles in the database
+app.get("/articles", function(req, res) {
+    db.Article.find({})
+        .then(function(dbArtile) {
+            res.json(dbArticle)
+        }).catch(function(err) {
+            res.json(err);
+        });
+});
+
+// get route for specific article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+        .populate("note")
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        }).catch(function(err) {
+            res.json(err);
+        });
+});
+
+// post route for saving/updating an article's note
+app.post("/articles/:id", function(req, res) {
+    db.Note.create(req.body)
+        .then(function(dbNote) {
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+        }).then(function(dbArticle) {
+            res.json(dbArticle);
+        }).catch(function(err) {
+            res.json(err);
+        });
+});
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT);
+});
